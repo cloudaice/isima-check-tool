9c9084e8a2c8870084ebc057b2594c2c10cb90a4
cloudaice <cloudaice@163.com>
2013-04-16 15:52:27 +0200

    init database

--- test/initdata.py
+++ test/initdata.py
@@ -1,6 +1,7 @@
 from pymongo import Connection
 from xlrd import open_workbook
 import random
+import datetime
 
 cnn = Connection("localhost", 27017)
 db = cnn["isima"]
@@ -27,6 +28,15 @@ def read_xls():
     return docs
 
 
+def get_date(year, month, day):
+    day = datetime.datetime(year, month, day)
+    while True:
+        interval = random.randint(1, 4)
+        next_day = day + datetime.timedelta(days=interval)
+        day = next_day
+        yield next_day.strftime("%Y-%m-%d")
+
+
 def view_data():
     docs = read_xls()
     for exame, lastname, firstname, student_username, section, teacher_username in docs:
@@ -44,6 +54,18 @@ def random_group():
     return groups[index]
 
 
+def random_year():
+    years = ["2012", "2013", "2014", "2015"]
+    index = random.randint(0, 3)
+    return years[index]
+
+
+def random_hour():
+    hours = ["08:00-10:00", "10:00-12:00", "08:00-09:30", "13:00-15:00", "15:00-17:00", "13:30-15:30"]
+    index = random.randint(0, 5)
+    return hours[index]
+
+
 def CreateStudent():
     table = db.Student
     data = read_xls()
@@ -53,7 +75,7 @@ def CreateStudent():
             "username": student_username,
             "firstname": firstname,
             "lastname": lastname,
-            "year": 2014,
+            "year": random_year(),
             "group": random_group(),
             "section": section,
             "origin": "FI"
@@ -85,44 +107,58 @@ def CreateCourse():
     table = db.Course
     data = read_xls()
     docs = list()
-    for exame, _, _, _, section, teacher_username in data:
+    for exame, _, _, student_username, section, teacher_username in data:
         docs.append({
-            "name": exame,
-            "sessions_num": 24,
-            "teacher": teacher_username,
+            "course_name": exame,
+            "teacher_name": teacher_username,
+            "year": "2013",
             "section": section,
+            "group": random_group(),
+            "origin": "FI",
+            "sessions_num": 24,
+            "students": [student_username
+                    for this_exame, _, _, student_username, section, this_teacher_username in data
+                    if exame == this_exame and teacher_username == this_teacher_username
+                         ]
         })
 
     for doc in docs:
         condition = {
-            "name": doc["name"],
-            "teacher": doc["teacher"],
+            "course_name": doc["course_name"],
+            "teacher_name": doc["teacher_name"],
+            "year": doc["year"]
         }
         table.update(condition, doc, upsert=True)
-        
+
 
 def CreateSession():
     table = db.Session
-    data = read_xls()
     docs = list()
-    for exame, _, _, _, _, _, in data:
-        docs.append({
-            "course": exame,
-            "date_hour": "2013-10-12-13:00",
-            "filled": False,
-            "missing_students": []
-        })
+    cursor = db.Course.find({}, {"course_name": 1, "teacher_name": 1, "year": 1})
+    for doc in cursor:
+        date = get_date(int(doc["year"]), 03, 23)
+        for _ in range(24):
+            docs.append({
+                "course_name": doc["course_name"],
+                "teacher_name": doc["teacher_name"],
+                "date": date.next(),
+                "interval_hour": random_hour(),
+                "filled": False,
+                "missing_students": []
+            })
     for doc in docs:
         condition = {
-            "course": doc['course'],
+            "course_name": doc["course_name"],
+            "teacher_name": doc["teacher_name"],
+            "date": doc["date"]
         }
         table.update(condition, doc, upsert=True)
 
 
 if __name__ == "__main__":
     #view_data()
-    CreateSession()
+    CreateStudent()
     CreateTeacher()
     CreateAdmin()
     CreateCourse()
-    CreateStudent()
+    CreateSession()
